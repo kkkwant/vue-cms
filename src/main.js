8ac45a2ee02b3bbf166194c3bb7d6eb4e2aeb02c
jin <649436311@qq.com>
2019-03-02 23:49:57 +0800

    vue-resouce before

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 // 入口文件
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
-import { Header } from 'mint-ui'
+
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+
+
+
+import router from './router.js'
 
 import app from './App.vue'
 
 var vm = new Vue({
 	el: '#app',
-	render: c => c(app)
+	render: c => c(app),
+	router
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,25 @@
 import VueRouter from 'vue-router'
 
+// 导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
+
+
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/shopcar', component: ShopcarContainer },
+    { path: '/search', component: SearchContainer },
+  ],
+  linkActiveClass: 'mui-active'
 })
 
 // 把路由对象暴露出去
